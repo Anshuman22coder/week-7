80b4fbe9bcaf1dae77a98b2357282bf481dd802c
RAJAN KANNAUJIYA <rajankannaujiya2034@gmail.com>
2024-05-16 12:50:14 +0530

    completed the Assignment

--- src/components/Q3.customR.jsx
+++ src/components/Q3.customR.jsx
@@ -1,17 +1,35 @@
 
 
 
-function CustomComponent(){
-
-    
+import { useEffect, useState } from "react";
 
 
-    return(
-        <>
-            CustomComponent
-        </>
-    )
+function createReactElement(){
 
+    const divToAppend = document.getElementById('customDivElement');
+    const anchorTag = document.createElement('a');
+
+    anchorTag.setAttribute('href', 'https://www.google.com')
+    anchorTag.textContent = 'checkout my github';
+
+    divToAppend.appendChild(anchorTag);
+}
+
+
+
+function CustomComponent() {
+    const [showAnchor, setShowAnchor] = useState(true);
+
+  useEffect(()=>{
+    if(showAnchor){
+        createReactElement();
+    }
+
+    setShowAnchor(false);
+  },[showAnchor])
+
+
+  return <div id="customDivElement"></div>;
 }
 
 export default CustomComponent;
